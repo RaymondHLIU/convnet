0cd402b62e0ea70301921faa1afd67acf05bf845
liuhao <liuhao@liuhao-T430u.(none)>
2014-06-07 20:50:37 +0800

    runnable version of cuda-convnet, running on CIFAR-10

---
+++ dummyinclude/cutil_inline.h
@@ -0,0 +1,8 @@
+/*
+Dummy Includes for cutil_inline.h
+*/
+#include <helper_cuda.h>
+#define cutilCheckMsg(a) getLastCudaError(a)
+#define cutGetMaxGflopsDeviceId() gpuGetMaxGflopsDeviceId()
+#define MIN(a,b) (a) < (b) ? (a) : (b)
+
